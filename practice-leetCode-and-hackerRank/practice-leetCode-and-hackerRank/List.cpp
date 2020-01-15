50ad3c1d58c3e5a73496c95372542624285a9973
Bryan Lopez <bryanjlopez01@gmail.com>
2020-01-15 13:26:17 -0800

    Added my initial version of singly Linked list

---
+++ practice-leetCode-and-hackerRank/practice-leetCode-and-hackerRank/List.cpp
@@ -0,0 +1,9 @@
+//
+//  List.cpp
+//  practice-leetCode-and-hackerRank
+//
+//  Created by Bryan Lopez on 1/15/20.
+//  Copyright © 2020 Lopez Bryan. All rights reserved.
+//
+
+#include "List.hpp"
---
+++ practice-leetCode-and-hackerRank/practice-leetCode-and-hackerRank/List.h
@@ -0,0 +1,14 @@
+//
+//  List.hpp
+//  practice-leetCode-and-hackerRank
+//
+//  Created by Bryan Lopez on 1/15/20.
+//  Copyright © 2020 Lopez Bryan. All rights reserved.
+//
+
+#ifndef List_hpp
+#define List_hpp
+
+#include <stdio.h>
+
+#endif /* List_hpp */
