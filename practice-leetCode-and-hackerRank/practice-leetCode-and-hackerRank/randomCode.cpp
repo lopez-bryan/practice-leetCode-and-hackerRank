28a3bcd11a376fdd3dcddcf671220606496e7a98
Bryan Lopez <bryanjlopez01@gmail.com>
2020-01-15 16:32:23 -0800

    Reverse int
    Reverse in integer code. Fully working with buffer overflow protection.

--- practice-leetCode-and-hackerRank/practice-leetCode-and-hackerRank/randomCode.cpp
+++ practice-leetCode-and-hackerRank/practice-leetCode-and-hackerRank/randomCode.cpp
@@ -10,48 +10,32 @@
 #include "randomCode.h"
 #include <cstdlib>
 
-// This function will reverse an integer passed through the parameters. This does not pass the leetCode answers.
-// The code for leetCode needs to have a long to handle the buffer overflow. I tried to add some restrictions,
-// but the asnwers in leetCode don't recognize them.
+// This function will reverse an integer passed through the parameters. I eventually got this answer from leetCode. 
 int RandomCode::reverseInt(int num) {
     // Used to store the new number
     int sum = 0;
-    int flag = 0;
-    int currentDigit = 0;
     
-    // First check and see if the input is negative or not. If it is, flag it and then make it positive
-    // using the absolut value function in cstlib header or cmath heading if a decimal
-    if (num < 0) {
-        flag = 1;
-        num = abs(num);
-    }
-    int previousNum = 0;
         
         // While num is not down to zero
-        while (num > 0) {
+        while (num != 0) {
             // By multiplying the sum by 10, we move the decimal over one place. Then we add that to the
             // last number being passed in (num in this case). We get that last number by frabbing it using the
             // modulo.
             
-            currentDigit = num % 10;
-            sum = (sum * 10) + currentDigit;
+            int remainder = num % 10;
+            num = num / 10;
             
             // Checking for buffer overflow. The value of the previous and current should be the same for their to
             // be no problem.
-            if ((sum - currentDigit) / 10 != previousNum)
-            {
-                return -1;
+            if (sum > INT_MAX / 10 || (sum == INT_MAX / 10 && remainder > 7)) {
+                return 0;
             }
-            else {
-                previousNum = sum;
-                // Now we just cut the last number off one by one by dividing it by 10.
-                num = num / 10;
+            if (sum < INT_MIN / 10 || (sum == INT_MIN / 10 && remainder < -8)) {
+                return 0;
             }
+            sum = (sum * 10) + remainder;
         }
-        // If we flagged it, we need to change it back to a negative number.
-        if (flag == 1) {
-            sum *= -1;
-        }
-    return sum;
+             return sum;
 }
+
 
